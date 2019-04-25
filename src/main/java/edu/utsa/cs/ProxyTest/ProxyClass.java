5c08349c6e95982c0b20648a9f8a7a1edfbcfc9f
kbaldor <kevin.baldor@gmail.com>
2019-04-25 17:09:14 -0500

    Added simple tests for branch detection.

--- src/main/java/edu/utsa/cs/ProxyTest/ProxyClass.java
+++ src/main/java/edu/utsa/cs/ProxyTest/ProxyClass.java
@@ -5,7 +5,9 @@ import java.lang.reflect.Method;
 
 public class ProxyClass implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if(method.getName().equals("methodB")) {
+        //if(method.getName().equals("methodB")) {
+        //if("methodA".equals("methodB")) {
+        if( 1 == 2) {
             IndirectCall ic = new IndirectCall();
             ic.call();
         } else {
